259d4d6c0a37d578d52b122c6c7245e5ddf2a28b
Tim Murphy <tim@26tp.com>
2014-09-27 01:04:10 +1000

    Added scripts to install psake package.

---
+++ Scripts/Support/Get-NuGetPath.ps1
@@ -0,0 +1,11 @@
+﻿$path = (Join-Path -Path $env:USERPROFILE -ChildPath "AppData\Local\NuGet\NuGet.exe")
+
+try {
+    Push-Location $PSScriptRoot
+    .\Validate-Path.ps1 -Path $path -ErrorMessage "Cannot find NuGet executable '$path'."
+}
+finally {
+    Pop-Location
+}
+
+return $path
