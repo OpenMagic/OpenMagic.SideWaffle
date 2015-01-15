2b661d03f535d03d89d37ea27eb77ba5884c9dbc
Tim Murphy <tim@26tp.com>
2015-01-16 04:36:27 +1100

    Updated NuGet packages and AssemlyInfos.

---
+++ Source/PortableClassLibraryMagic/Properties/CommonAssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// todo: Update
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright © 2015 Tim Murphy")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
--- Source/SideWaffleMagic/Properties/AssemblyInfo.cs
+++ Source/SideWaffleMagic/Properties/AssemblyInfo.cs
@@ -4,7 +4,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SideWaffleMagic")]
 [assembly: AssemblyDescription("Collection of SideWaffle templates.")]
 [assembly: AssemblyProduct("SideWaffleMagic")]
-[assembly: AssemblyCopyright("Copyright (c) 2014 Tim Murphy")]
+[assembly: AssemblyCopyright("Copyright (c) 2014-2015 Tim Murphy")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
